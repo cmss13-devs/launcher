7f78ea305538c4b01e8983e48a2d7c244d556d66
harryob <55142896+harryob@users.noreply.github.com>
2026-04-23 18:20:27 +0100

    critical react error

--- src/components/DirectConnectModal.tsx
+++ src/components/DirectConnectModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
